0497fa280ef304c16c5ec0d60cf9c046cbfe2669
Pai18 <anhquyen18092000@gmail.com>
2024-06-06 08:30:18 +0700

    refactor: reorganize user logs tables in database

--- database/migrations/2024_04_12_145419_create_user_policies_table.php
+++ database/migrations/2024_04_12_145419_create_user_policies_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_policies', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('policy_id')->constrained('policies')->onDelete('cascade');
+            $table->primary(['user_id', 'policy_id']);
         });
     }
 
---
+++ database/migrations/2024_06_06_082343_create_user_logs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('action');
+            $table->timestamps();
+            $table->morphs('log');
+            $table->foreignId('user_id')->constrained('users')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_logs', function (Blueprint $table) {
+            $table->dropForeign('user_logs_user_id_foreign');
+        });
+        Schema::dropIfExists('user_logs');
+    }
+};
